74f09d8a3cfd6c9f7d25e139f8813079bfabd37c
SAYAN KOLEY <120122201+sayan110923@users.noreply.github.com>
2025-07-17 10:20:25 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -17,17 +17,3 @@ connectDB().then(() => {
     console.log(`✅ Server is running on http://localhost:${port}`);
   });
 });
-
-
-
-
-// {
-//     "message": "Login successful",
-//     "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NzBjNGFiNTdlYjZmMWEzMDUwMmYwMSIsImlhdCI6MTc1MjQzMjk3OCwiZXhwIjoxNzUyNDQwMTc4fQ.7hP69KuvzutmkT4zUYNZP_LwB1jE63OKHzzbNsCCF2M",
-//     "refreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NzBjNGFiNTdlYjZmMWEzMDUwMmYwMSIsImlhdCI6MTc1MjQzMjk3OCwiZXhwIjoxNzUzMjk2OTc4fQ.IgdfDrOTzb8-PByjkGuoPCjt4j-1JGXBRO1-IJolR8U",
-//     "user": {
-//         "id": "6870c4ab57eb6f1a30502f01",
-//         "name": "Aman Singh",
-//         "email": "aman1@gmail.com"
-//     }
-// }
